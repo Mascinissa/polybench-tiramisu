38beeae75f798f2a31fdc034ecca691ac34835dc
Mascinissa <fm_merouani@esi.dz>
2021-01-13 14:24:03 +0100

    Added correlation benchmark

---
+++ datamining/correlation/correlation_generator.cpp
@@ -0,0 +1,87 @@
+#include <tiramisu/tiramisu.h>
+#include "benchmarks.h"
+// #define eps 0.1
+
+using namespace tiramisu;
+
+/*
+   TODO          
+*/
+
+int main(int argc, char **argv)
+{
+    tiramisu::init("correlation");
+
+    // -------------------------------------------------------
+    // Layer I
+    // ------------------------------------------------------- 
+
+    //Iteration variables    
+    var i("i", 0, M), j("j", 0, M), k("k", 0, N), l("l", 0, N);
+    
+
+    //inputs
+    input data("data", {l, j}, p_float64);
+
+
+    //Computations
+    
+    computation mean_init("mean_init", {j}, 0.0);
+    computation mean("mean", {l,j}, p_float64);
+    mean.set_expression(mean(l,j) + data(l,j)/expr(cast(p_float64, N)));
+    
+    computation cov_init("cov_init", {i,j}, 0.0);
+    computation cov("cov", {i,j,k}, p_float64);
+    cov.set_expression(cov(i,j,k) + (data(k,i)-mean(0,i))*(data(k,j)-mean(0,j))/expr(cast(p_float64, N)));
+
+    computation variance_init("variance_init", {i}, 0.0);
+    computation variance("variance", {k, i}, p_float64);
+    variance.set_expression(variance(k, i) + (data(k,i) - mean(0,i))*(data(k,i) - mean(0,i))/expr(cast(p_float64, N)));
+    computation std("std", {i}, expr(o_sqrt, variance(0, i)));
+    // computation std("std", {i}, expr(o_max, eps, expr(o_sqrt, variance(0, i))));
+
+    computation corr("corr", {i, j}, cov(i,j,0)/(std(i)*std(j)));
+
+    // -------------------------------------------------------
+    // Layer II
+    // -------------------------------------------------------
+    mean_init.then(mean, computation::root)
+             .then(cov_init, computation::root)
+             .then(cov, computation::root)
+             .then(variance_init, computation::root)
+             .then(variance, computation::root)
+             .then(std, computation::root)
+             .then(corr, computation::root);
+
+    // -------------------------------------------------------
+    // Layer III
+    // -------------------------------------------------------
+    //Input Buffers
+    buffer b_data("b_data", {N,M}, p_float64, a_input);
+    buffer b_mean("b_mean", {M}, p_float64, a_temporary);
+    buffer b_std("b_std", {M}, p_float64, a_temporary);
+    buffer b_cov("b_cov", {M,M}, p_float64, a_temporary);   
+    buffer b_corr("b_corr", {M,M}, p_float64, a_output);   
+    
+
+    //Store inputs
+    data.store_in(&b_data);
+    
+
+    //Store computations
+    mean_init.store_in(&b_mean);
+    mean.store_in(&b_mean, {j});
+    variance_init.store_in(&b_std);
+    variance.store_in(&b_std, {i});
+    std.store_in(&b_std);
+    cov_init.store_in(&b_cov);
+    cov.store_in(&b_cov, {i,j});
+    corr.store_in(&b_corr);
+
+    // -------------------------------------------------------
+    // Code Generation
+    // -------------------------------------------------------
+    tiramisu::codegen({&b_data, &b_corr}, "generated_correlation.o");
+
+    return 0;
+}
---
+++ datamining/correlation/correlation_wrapper.cpp
@@ -0,0 +1,154 @@
+#include <Halide.h>
+#include <tiramisu/tiramisu.h>
+#include <iostream>
+#include "generated_correlation.o.h"
+#include "benchmarks.h"
+#include <tiramisu/utils.h>
+
+
+int correlation_ref(Halide::Buffer<double> data, Halide::Buffer<double> corr)
+{
+    Halide::Buffer<double> mean(M),stddev(M);
+    int i,j,k;
+    // double  eps = 0.1;
+
+   for (j = 0; j < M; j++)
+    {
+      mean(j) = (0.0);
+      for (i = 0; i < N; i++)
+	    mean(j) += data(i, j);
+      mean(j) /= N;
+    }
+
+
+   for (j = 0; j < M; j++)
+    {
+      stddev(j) = (0.0);
+      for (i = 0; i < N; i++)
+        stddev(j) += (data(i, j) - mean(j)) * (data(i, j) - mean(j));
+      stddev(j) /= N;
+      stddev(j) = sqrt(stddev(j));
+      /* The following in an inelegant but usual way to handle
+         near-zero std. dev. values, which below would cause a zero-
+         divide. */
+      // stddev(j) = stddev(j) <= eps ? (1.0) : stddev(j);
+    }
+
+  /* Center and reduce the column vectors. */
+  for (i = 0; i < N; i++)
+    for (j = 0; j < M; j++)
+      {
+        data(i, j) -= mean(j);
+        data(i, j) /= sqrt(N) * stddev(j);
+      }
+
+  /* Calculate the m * m correlation matrix. */
+  for (i = 0; i < M-1; i++)
+    {
+      corr(i, i) = (1.0);
+      for (j = i+1; j < M; j++)
+        {
+          corr(i, j) = (0.0);
+          for (k = 0; k < N; k++)
+            corr(i, j) += (data(k, i) * data(k, j));
+          corr(j, i) = corr(i, j);
+        }
+    }
+  corr(M-1, M-1) = (1.0);
+
+  return 0;
+}
+
+int main(int argc, char** argv)
+{
+    std::vector<std::chrono::duration<double, std::milli>> duration_vector_1, duration_vector_2;
+    bool run_ref = false, run_tiramisu = false;
+
+    const char* env_ref = std::getenv("RUN_REF");
+
+    if (env_ref != NULL && env_ref[0] == '1')
+        run_ref = true;
+
+    const char* env_tiramisu = std::getenv("RUN_TIRAMISU");
+
+    if (env_tiramisu != NULL && env_tiramisu[0] == '1')
+        run_tiramisu = true;
+
+    // ---------------------------------------------------------------------
+    // ---------------------------------------------------------------------
+    // ---------------------------------------------------------------------
+    Halide::Buffer<double> b_data(M,N), b_corr(M,M), b_corr_ref(M, M);
+    // ---------------------------------------------------------------------
+    // ---------------------------------------------------------------------
+    // ---------------------------------------------------------------------
+
+    //REFERENCE
+    {
+        for (int i = 0; i < NB_TESTS; ++i)
+        {
+          init_buffer(b_corr_ref, (double) 1);
+	        // init_buffer(b_data, (double) 2);
+          for (int i = 0; i < M; ++i)  
+            for (int j = 0; j < N; ++j)
+              b_data(j,i)=pow(-1,(j%2))*i+pow(-1,((j+i)%2))*j;
+          
+          transpose(b_data);
+          transpose(b_corr_ref);
+          auto start = std::chrono::high_resolution_clock::now();
+
+	        if (run_ref)
+	    	    correlation_ref(b_data, b_corr_ref);
+
+	        auto end = std::chrono::high_resolution_clock::now();
+          transpose(b_data);
+          transpose(b_corr_ref);
+          
+          duration_vector_1.push_back(end - start);
+        }
+    }
+
+    //TIRAMISU
+    {
+        for (int i = 0; i < NB_TESTS; ++i)
+        {
+          init_buffer(b_corr, (double) 1);
+	        // init_buffer(b_data, (double) 2);
+          for (int i = 0; i < M; ++i)  
+            for (int j = 0; j < N; ++j)
+              b_data(j,i)=pow(-1,(j%2))*i+pow(-1,((j+i)%2))*j;
+          
+          auto start = std::chrono::high_resolution_clock::now();
+
+	        if (run_tiramisu)
+	    	    correlation(b_data.raw_buffer(), b_corr.raw_buffer());
+
+	        auto end = std::chrono::high_resolution_clock::now();
+          duration_vector_2.push_back(end - start);
+        }
+    }
+    /***************************/
+    print_buffer(b_data);
+std::cout << "Tiramisu " << std::endl;
+        print_buffer(b_corr);
+        std::cout << "Reference " << std::endl;
+        print_buffer(b_corr_ref);
+        std::cout << b_corr(1,0) - b_corr_ref(1,0)<< std::endl;
+    /******************************/
+
+    print_time("performance_cpu.csv", "correlation",
+	       {"Ref", "Tiramisu"},
+	       {median(duration_vector_1), median(duration_vector_2)});
+
+    if (CHECK_CORRECTNESS && run_ref && run_tiramisu)
+        compare_buffers_approximately("correlation", b_corr_ref, b_corr, 0.001);
+
+    if (PRINT_OUTPUT)
+    {
+        std::cout << "Tiramisu " << std::endl;
+        print_buffer(b_corr);
+        std::cout << "Reference " << std::endl;
+        print_buffer(b_corr_ref);
+    }
+
+    return 0;
+}
