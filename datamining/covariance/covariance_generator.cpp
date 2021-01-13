717351b8f10b71b33c9442c023044210dce92076
Mascinissa <fm_merouani@esi.dz>
2021-01-13 10:14:14 +0100

    Added covariance benchmark

---
+++ datamining/covariance/covariance_generator.cpp
@@ -0,0 +1,68 @@
+#include <tiramisu/tiramisu.h>
+#include "benchmarks.h"
+
+using namespace tiramisu;
+
+/*
+   TODO          
+*/
+
+int main(int argc, char **argv)
+{
+    tiramisu::init("covariance");
+
+    // -------------------------------------------------------
+    // Layer I
+    // ------------------------------------------------------- 
+
+    //Iteration variables    
+    var i("i", 0, M), j("j", 0, M), k("k", 0, N), l("l", 0, N);
+    
+
+    //inputs
+    input data("data", {l, j}, p_float64);
+
+
+    //Computations
+    
+    computation mean_init("mean_init", {j}, 0.0);
+    computation mean("mean", {l,j}, p_float64);
+    mean.set_expression(mean(l,j) + data(l,j)/expr(cast(p_float64, N)));
+    
+    computation cov_init("conv_init", {i,j}, 0.0);
+    computation cov("cov", {i,j,k}, p_float64);
+    cov.set_expression(cov(i,j,k) + (data(k,i)-mean(0,i))*(data(k,j)-mean(0,j))/expr(cast(p_float64, N-1)));
+
+    // -------------------------------------------------------
+    // Layer II
+    // -------------------------------------------------------
+    mean_init.then(mean, computation::root)
+             .then(cov_init, computation::root)
+             .then(cov, computation::root);
+
+    // -------------------------------------------------------
+    // Layer III
+    // -------------------------------------------------------
+    //Input Buffers
+    buffer b_data("b_data", {N,M}, p_float64, a_input);
+    buffer b_mean("b_mean", {M}, p_float64, a_temporary);
+    buffer b_cov("b_cov", {M,M}, p_float64, a_output);   
+    
+
+    //Store inputs
+    data.store_in(&b_data);
+    
+
+    //Store computations
+    mean_init.store_in(&b_mean);
+    mean.store_in(&b_mean, {j});
+    cov_init.store_in(&b_cov);
+    cov.store_in(&b_cov, {i,j});
+
+    // -------------------------------------------------------
+    // Code Generation
+    // -------------------------------------------------------
+    tiramisu::codegen({&b_data, &b_cov}, "generated_covariance.o");
+
+    return 0;
+}
---
+++ datamining/covariance/covariance_wrapper.cpp
@@ -0,0 +1,111 @@
+#include <Halide.h>
+#include <tiramisu/tiramisu.h>
+#include <iostream>
+#include "generated_covariance.o.h"
+#include "benchmarks.h"
+#include <tiramisu/utils.h>
+
+//TODO
+int covariance_ref(Halide::Buffer<double> data, Halide::Buffer<double> cov)
+{
+    Halide::Buffer<double> mean(M);
+    int i,j,k;
+
+    for (j = 0; j < M; j++)
+    {
+      mean(j)=0;
+      for (i = 0; i < N; i++)
+        mean(j) += data(j,i);
+      mean(j) /= N;
+    }
+
+  for (i = 0; i < N; i++)
+    for (j = 0; j < M; j++)
+      data(j,i) -= mean(j);
+
+  for (i = 0; i < M; i++)
+    for (j = i; j < M; j++)
+      {
+        cov(j,i) = 0;
+        for (k = 0; k < N; k++)
+	  cov(j,i) += data(i,k) * data(j,k);
+        cov(j,i) /= (N - 1);
+        cov(i,j) = cov(j,i);  
+      }
+    return 0;
+}
+
+int main(int argc, char** argv)
+{
+    std::vector<std::chrono::duration<double, std::milli>> duration_vector_1, duration_vector_2;
+    bool run_ref = false, run_tiramisu = false;
+
+    const char* env_ref = std::getenv("RUN_REF");
+
+    if (env_ref != NULL && env_ref[0] == '1')
+        run_ref = true;
+
+    const char* env_tiramisu = std::getenv("RUN_TIRAMISU");
+
+    if (env_tiramisu != NULL && env_tiramisu[0] == '1')
+        run_tiramisu = true;
+
+    // ---------------------------------------------------------------------
+    // ---------------------------------------------------------------------
+    // ---------------------------------------------------------------------
+    Halide::Buffer<double> b_data(M,N), b_cov(M,M), b_cov_ref(M, M);
+    // ---------------------------------------------------------------------
+    // ---------------------------------------------------------------------
+    // ---------------------------------------------------------------------
+
+    //REFERENCE
+    {
+        for (int i = 0; i < NB_TESTS; ++i)
+        {
+            init_buffer(b_cov_ref, (double) 1);
+	        init_buffer(b_data, (double) 2);
+            b_data(5,2)=52;
+            auto start = std::chrono::high_resolution_clock::now();
+
+	        if (run_ref)
+	    	    covariance_ref(b_data, b_cov_ref);
+
+	        auto end = std::chrono::high_resolution_clock::now();
+            duration_vector_1.push_back(end - start);
+        }
+    }
+
+    //TIRAMISU
+    {
+        for (int i = 0; i < NB_TESTS; ++i)
+        {
+            init_buffer(b_cov, (double) 1);
+	        init_buffer(b_data, (double) 2);
+            b_data(5,2)=52;
+            auto start = std::chrono::high_resolution_clock::now();
+
+	        if (run_tiramisu)
+	    	    covariance(b_data.raw_buffer(), b_cov.raw_buffer());
+
+	        auto end = std::chrono::high_resolution_clock::now();
+            duration_vector_2.push_back(end - start);
+        }
+    }
+
+    print_time("performance_cpu.csv", "covariance",
+	       {"Ref", "Tiramisu"},
+	       {median(duration_vector_1), median(duration_vector_2)});
+
+    if (CHECK_CORRECTNESS && run_ref && run_tiramisu)
+        compare_buffers("covariance", b_cov_ref, b_cov);
+
+    if (PRINT_OUTPUT)
+    {
+        std::cout << "Tiramisu " << std::endl;
+        print_buffer(b_cov);
+        std::cout << "Reference " << std::endl;
+        print_buffer(b_cov_ref);
+    }
+
+    return 0;
+}
