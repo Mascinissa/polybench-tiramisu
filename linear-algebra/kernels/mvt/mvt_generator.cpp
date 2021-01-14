6da972aeafccb5f044eb21e2c78889c442e443ac
Mascinissa <fm_merouani@esi.dz>
2021-01-14 18:38:36 +0100

    Added mvt benchmark

---
+++ linear-algebra/kernels/mvt/mvt_generator.cpp
@@ -0,0 +1,64 @@
+#include <tiramisu/tiramisu.h>
+#include "benchmarks.h"
+
+
+using namespace tiramisu;
+
+/*
+   TODO description
+*/
+
+int main(int argc, char **argv) {
+  tiramisu::init("mvt");
+
+  // -------------------------------------------------------
+  // Layer I
+  // -------------------------------------------------------
+
+  // Iteration variables
+  var i("i", 0, N), j("j", 0, N);
+
+  // inputs
+  input A("A", {i, j}, p_float64);
+  input y1("y1", {j}, p_float64);
+  input y2("y2", {j}, p_float64);
+
+  // Computations
+  computation x1("x1", {i, j}, p_float64);
+  x1.set_expression(x1(i, j) + A(i, j) * y1(j));
+  computation x2("x2", {i, j}, p_float64);
+  x2.set_expression(x2(i, j) + A(j, i) * y2(j));
+
+
+  // -------------------------------------------------------
+  // Layer II
+  // -------------------------------------------------------
+  x1.then(x2, computation::root);
+
+  // -------------------------------------------------------
+  // Layer III
+  // -------------------------------------------------------
+  // Input Buffers
+  buffer b_A("b_A", {N, N}, p_float64, a_input);
+  buffer b_y1("b_y1", {N}, p_float64, a_input);
+  buffer b_y2("b_y2", {N}, p_float64, a_input);
+  buffer b_x1("b_x1", {N}, p_float64, a_output);
+  buffer b_x2("b_x2", {N}, p_float64, a_output);
+
+  // Store inputs
+  A.store_in(&b_A);
+  y1.store_in(&b_y1);
+  y2.store_in(&b_y2);
+
+
+  // Store computations
+  x1.store_in(&b_x1, {i});
+  x2.store_in(&b_x2, {i});
+
+  // -------------------------------------------------------
+  // Code Generation
+  // -------------------------------------------------------
+  tiramisu::codegen({&b_A, &b_y1, &b_y2, &b_x1, &b_x2}, "generated_mvt.o");
+
+  return 0;
+}
---
+++ linear-algebra/kernels/mvt/mvt_wrapper.cpp
@@ -0,0 +1,102 @@
+#include <Halide.h>
+#include <tiramisu/tiramisu.h>
+#include <iostream>
+#include "generated_mvt.o.h"
+#include "benchmarks.h"
+#include <tiramisu/utils.h>
+
+int mvt_ref(Halide::Buffer<double> A, Halide::Buffer<double> y1, Halide::Buffer<double> y2, Halide::Buffer<double> x1, Halide::Buffer<double> x2)
+{
+    int i, j;
+
+    for (i = 0; i < N; i++)
+        for (j = 0; j < N; j++)
+            x1(i) = x1(i) + A(i, j) * y1(j);
+    for (i = 0; i < N; i++)
+        for (j = 0; j < N; j++)
+            x2(i) = x2(i) + A(j, i) * y2(j);
+
+    return 0;
+}
+
+int main(int argc, char** argv)
+{
+    std::vector<std::chrono::duration<double, std::milli> > duration_vector_1,
+        duration_vector_2;
+    bool run_ref = false, run_tiramisu = false;
+
+    const char* env_ref = std::getenv("RUN_REF");
+
+    if (env_ref != NULL && env_ref[0] == '1')
+        run_ref = true;
+
+    const char* env_tiramisu = std::getenv("RUN_TIRAMISU");
+
+    if (env_tiramisu != NULL && env_tiramisu[0] == '1')
+        run_tiramisu = true;
+
+    // ---------------------------------------------------------------------
+    // ---------------------------------------------------------------------
+    // ---------------------------------------------------------------------
+    Halide::Buffer<double> b_A(N, N), b_y1(N), b_y2(N), b_x1_ref(N), b_x2_ref(N), b_x1(N), b_x2(N);
+    // ---------------------------------------------------------------------
+    // ---------------------------------------------------------------------
+    // ---------------------------------------------------------------------
+
+    // REFERENCE
+    {
+        for (int i = 0; i < NB_TESTS; ++i) {
+            init_buffer(b_x1_ref, (double)1);
+            init_buffer(b_x2_ref, (double)2);
+            init_buffer(b_A, (double)3);
+            init_buffer(b_y1, (double)4);
+            init_buffer(b_y2, (double)5);
+
+            transpose(b_A);
+            auto start = std::chrono::high_resolution_clock::now();
+
+            if (run_ref)
+                mvt_ref(b_A, b_y1, b_y2, b_x1_ref, b_x2_ref);
+
+            auto end = std::chrono::high_resolution_clock::now();
+            transpose(b_A);
+
+            duration_vector_1.push_back(end - start);
+        }
+    }
+
+    // TIRAMISU
+    {
+        for (int i = 0; i < NB_TESTS; ++i) {
+            init_buffer(b_x1, (double)1);
+            init_buffer(b_x2, (double)2);
+            init_buffer(b_A, (double)3);
+            init_buffer(b_y1, (double)4);
+            init_buffer(b_y2, (double)5);
+
+            auto start = std::chrono::high_resolution_clock::now();
+            if (run_tiramisu)
+                mvt(b_A.raw_buffer(), b_y1.raw_buffer(), b_y2.raw_buffer(), b_x1.raw_buffer(), b_x2.raw_buffer());
+
+            auto end = std::chrono::high_resolution_clock::now();
+            duration_vector_2.push_back(end - start);
+        }
+    }
+
+
+    print_time("performance_cpu.csv", "mvt", { "Ref", "Tiramisu" },
+        { median(duration_vector_1), median(duration_vector_2) });
+
+    if (CHECK_CORRECTNESS && run_ref && run_tiramisu)
+        compare_buffers("mvt x1", b_x1_ref, b_x1);
+        compare_buffers("mvt x2", b_x2_ref, b_x2);
+
+    if (PRINT_OUTPUT) {
+        std::cout << "Tiramisu " << std::endl;
+        print_buffer(b_x1);
+        std::cout << "Reference " << std::endl;
+        print_buffer(b_x1_ref);
+    }
+
+    return 0;
+}
