3a396e9d03fefcd21e9addce1bc7fee11885964a
Mascinissa <fm_merouani@esi.dz>
2021-01-15 11:39:04 +0100

    Added symm benchmark

---
+++ linear-algebra/blas/symm/symm_generator.cpp
@@ -0,0 +1,76 @@
+#include <tiramisu/tiramisu.h>
+#include "benchmarks.h"
+
+using namespace tiramisu;
+
+/*
+   TODO description
+*/
+
+int main(int argc, char **argv)
+{
+    tiramisu::init("symm");
+    // function symm("symm");
+
+    // -------------------------------------------------------
+    // Layer I
+    // ------------------------------------------------------- 
+    constant NN("NN", N);
+    constant MM("MM", M);
+
+    //Iteration variables    
+    var i("i", 0, M), j("j", 0, N), k("k", 0, M);
+    
+
+    //inputs
+    input A("A", {i, k}, p_float64);
+    input B("B", {i, j}, p_float64);
+    input C("C", {i, j}, p_float64);
+
+
+    //Computations
+    
+    computation temp_init("temp_init", {i,j}, 0.0);
+    computation temp("[MM,NN]->{temp[i,j,k]: 0<=i<MM and 0<=j<NN and 0<=k<i}", expr(), true, p_float64, global::get_implicit_function());
+    temp.set_expression(temp(i,j,k)+A(i,k)*B(k,j));
+    computation C_init("C_init", {i,j}, C(i, j)*beta+B(i, j)*A(i, i)*alpha+temp(i,j,0)*alpha);
+    computation C_out("[MM,NN]->{C_out[i,k,j]: 0<=i<MM and 0<=k<i and 0<=j<NN}", expr(), true, p_float64, global::get_implicit_function());
+    C_out.set_expression(C_out(i,k,j)+A(i,k)*B(k,j)*alpha);
+    
+    // -------------------------------------------------------
+    // Layer II
+    // -------------------------------------------------------
+    temp_init.then(temp, computation::root)
+             .then(C_init, computation::root)
+             .then(C_out, computation::root);
+
+    // -------------------------------------------------------
+    // Layer III
+    // -------------------------------------------------------
+    //Input Buffers
+    buffer b_A("b_A", {M,M}, p_float64, a_input);
+    buffer b_B("b_B", {M,N}, p_float64, a_input);
+    buffer b_C("b_C", {M,N}, p_float64, a_output);
+    buffer b_temp("b_temp", {M,N}, p_float64, a_temporary);
+    // buffer b_C_out("b_C_out", {P,R}, p_float64, a_output); 
+    
+
+    //Store inputs
+    A.store_in(&b_A);
+    B.store_in(&b_B);
+    C.store_in(&b_C);
+    
+
+    //Store computations
+    temp_init.store_in(&b_temp);
+    temp.store_in(&b_temp, {i,j});
+    C_init.store_in(&b_C);
+    C_out.store_in(&b_C, {k,j});
+
+    // -------------------------------------------------------
+    // Code Generation
+    // -------------------------------------------------------
+    tiramisu::codegen({&b_A, &b_B, &b_C}, "generated_symm.o");
+
+    return 0;
+}
---
+++ linear-algebra/blas/symm/symm_wrapper.cpp
@@ -0,0 +1,109 @@
+#include <Halide.h>
+#include <tiramisu/tiramisu.h>
+#include <iostream>
+#include "generated_symm.o.h"
+#include "benchmarks.h"
+#include <tiramisu/utils.h>
+
+
+int symm_ref(Halide::Buffer<double> A, Halide::Buffer<double> B, Halide::Buffer<double> C)
+{
+  int i,j,k,temp2;
+  for (i = 0; i < M; i++)
+    for (j = 0; j < N; j++) {
+        temp2 = 0;
+        for (k = 0; k < i; k++) {
+            C(k, j) += alpha * B(i, j) * A(i, k);
+            temp2 += B(k, j) * A(i, k);
+        }
+        C(i, j) = beta * C(i, j) + alpha * B(i, j) * A(i, i) + alpha * temp2;
+    }
+  return 0;
+}
+
+int main(int argc, char** argv)
+{
+    std::vector<std::chrono::duration<double, std::milli>> duration_vector_1, duration_vector_2;
+    bool run_ref = false, run_tiramisu = false;
+
+    const char* env_ref = std::getenv("RUN_REF");
+
+    if (env_ref != NULL && env_ref[0] == '1')
+        run_ref = true;
+
+    const char* env_tiramisu = std::getenv("RUN_TIRAMISU");
+
+    if (env_tiramisu != NULL && env_tiramisu[0] == '1')
+        run_tiramisu = true;
+
+    // ---------------------------------------------------------------------
+    // ---------------------------------------------------------------------
+    // ---------------------------------------------------------------------
+    Halide::Buffer<double> b_A(M,M), b_B(N,M), b_C(N,M), b_C_ref(N,M);
+    // ---------------------------------------------------------------------
+    // ---------------------------------------------------------------------
+    // ---------------------------------------------------------------------
+
+    //REFERENCE
+    {
+        for (int i = 0; i < NB_TESTS; ++i)
+        {
+	      init_buffer(b_A, (double) 2);
+	      init_buffer(b_B, (double) 4);
+          for (int i = 0; i < M; ++i)  
+            for (int j = 0; j < N; ++j)
+              b_C_ref(j,i)=pow(-1,(j%2))*i+pow(-1,((j+i)%2))*j;
+
+          transpose(b_C_ref);
+          transpose(b_A);
+          transpose(b_B);
+          auto start = std::chrono::high_resolution_clock::now();
+
+	        if (run_ref)
+	    	    symm_ref(b_A, b_B, b_C_ref);
+
+	        auto end = std::chrono::high_resolution_clock::now();
+          transpose(b_C_ref);
+          transpose(b_A);
+          transpose(b_B);
+          
+          duration_vector_1.push_back(end - start);
+        }
+    }
+
+    //TIRAMISU
+    {
+        for (int i = 0; i < NB_TESTS; ++i)
+        {
+	      init_buffer(b_A, (double) 2);
+	      init_buffer(b_B, (double) 4);
+          for (int i = 0; i < M; ++i)  
+            for (int j = 0; j < N; ++j)
+              b_C(j,i)=pow(-1,(j%2))*i+pow(-1,((j+i)%2))*j;
+
+          auto start = std::chrono::high_resolution_clock::now();
+	        if (run_tiramisu)
+	    	    symm(b_A.raw_buffer(), b_B.raw_buffer(), b_C.raw_buffer());
+
+	      auto end = std::chrono::high_resolution_clock::now();
+          duration_vector_2.push_back(end - start);
+        }
+    }
+
+    print_time("performance_cpu.csv", "symm",
+	       {"Ref", "Tiramisu"},
+	       {median(duration_vector_1), median(duration_vector_2)});
+
+    if (CHECK_CORRECTNESS && run_ref && run_tiramisu)
+        compare_buffers("symm", b_C_ref, b_C);
+
+    if (PRINT_OUTPUT)
+    {
+        std::cout << "Tiramisu " << std::endl;
+        print_buffer(b_C);
+        std::cout << "Reference " << std::endl;
+        print_buffer(b_C_ref);
+    }
+
+    return 0;
+}
