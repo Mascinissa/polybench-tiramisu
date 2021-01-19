47d68d755b7d6f6159582fec86fbf9cfebdaf5fa
Mascinissa <fm_merouani@esi.dz>
2021-01-19 12:10:37 +0100

    Added floyd_warshall benchmark

---
+++ Medley/floyd_warshall/floyd_warshall_generator.cpp
@@ -0,0 +1,52 @@
+#include <tiramisu/tiramisu.h>
+#include "benchmarks.h"
+
+using namespace tiramisu;
+
+/*
+   TODO description
+*/
+
+int main(int argc, char **argv)
+{
+    tiramisu::init("floyd_warshall");
+
+    // -------------------------------------------------------
+    // Layer I
+    // ------------------------------------------------------- 
+
+    //Iteration variables    
+    var i("i", 0, N), j("j", 0, N), k("k", 0, N);
+    
+    //inputs
+    input paths("paths", {i, j}, p_float64);
+    input B("B", {i, j}, p_float64);
+
+    //Computations
+    computation paths_update("paths_update", {k,i,j}, p_float64);
+    paths_update.set_expression(expr(o_min, paths(i,j), paths(i,k) + paths(k,j)));
+
+    // -------------------------------------------------------
+    // Layer II
+    // -------------------------------------------------------
+    // no_schedule
+    
+    // -------------------------------------------------------
+    // Layer III
+    // -------------------------------------------------------
+    //Input Buffers
+    buffer b_paths("b_paths", {N,N}, p_float64, a_output);    
+
+    //Store inputs
+    paths.store_in(&b_paths);
+
+    //Store computations
+    paths_update.store_in(&b_paths, {i,j});
+
+    // -------------------------------------------------------
+    // Code Generation
+    // -------------------------------------------------------
+    tiramisu::codegen({&b_paths}, "generated_floyd_warshall.o");
+
+    return 0;
+}
---
+++ Medley/floyd_warshall/floyd_warshall_wrapper.cpp
@@ -0,0 +1,109 @@
+#include <Halide.h>
+#include <tiramisu/tiramisu.h>
+#include <iostream>
+#include "generated_floyd_warshall.o.h"
+#include "benchmarks.h"
+#include <tiramisu/utils.h>
+
+
+int floyd_warshall_ref(Halide::Buffer<double> path)
+{
+  int i,j,k;
+
+  for (k = 0; k < N; k++)
+    {
+      for(i = 0; i < N; i++)
+        for (j = 0; j < N; j++)
+            path(i, j) = path(i, j) < (path(i, k) + path(k, j)) ? path(i, j) : path(i, k) + path(k, j);
+    }
+  return 0;
+}
+
+int init_array (Halide::Buffer<double> path)
+{
+  int i, j;
+  transpose(path);
+  for (i = 0; i < N; i++)
+    for (j = 0; j < N; j++) {
+      path(i, j) = i*j%7+1;
+      if ((i+j)%13 == 0 || (i+j)%7==0 || (i+j)%11 == 0)
+         path(i, j) = 999;
+    }
+  transpose(path);
+  return 0;
+}
+
+int main(int argc, char** argv)
+{
+    std::vector<std::chrono::duration<double, std::milli>> duration_vector_1, duration_vector_2;
+    bool run_ref = false, run_tiramisu = false;
+
+    const char* env_ref = std::getenv("RUN_REF");
+
+    if (env_ref != NULL && env_ref[0] == '1')
+        run_ref = true;
+
+    const char* env_tiramisu = std::getenv("RUN_TIRAMISU");
+
+    if (env_tiramisu != NULL && env_tiramisu[0] == '1')
+        run_tiramisu = true;
+
+    // ---------------------------------------------------------------------
+    // ---------------------------------------------------------------------
+    // ---------------------------------------------------------------------
+    Halide::Buffer<double> b_paths(N,N), b_paths_ref(N,N);
+    // ---------------------------------------------------------------------
+    // ---------------------------------------------------------------------
+    // ---------------------------------------------------------------------
+
+    //REFERENCE
+    {
+        for (int i = 0; i < NB_TESTS; ++i)
+        {
+
+	      init_array(b_paths_ref);
+          transpose(b_paths_ref);
+          auto start = std::chrono::high_resolution_clock::now();
+
+          if (run_ref)
+            floyd_warshall_ref(b_paths_ref);
+
+          auto end = std::chrono::high_resolution_clock::now();
+          transpose(b_paths_ref);
+          
+          duration_vector_1.push_back(end - start);
+        }
+    }
+
+    //TIRAMISU
+    {
+        for (int i = 0; i < NB_TESTS; ++i)
+        {
+	      init_array(b_paths);
+
+          auto start = std::chrono::high_resolution_clock::now();
+	      if (run_tiramisu)
+	        floyd_warshall(b_paths.raw_buffer());
+
+	      auto end = std::chrono::high_resolution_clock::now();
+          duration_vector_2.push_back(end - start);
+        }
+    }
+
+    print_time("performance_cpu.csv", "floyd_warshall",
+	       {"Ref", "Tiramisu"},
+	       {median(duration_vector_1), median(duration_vector_2)});
+
+    if (CHECK_CORRECTNESS && run_ref && run_tiramisu)
+        compare_buffers("floyd_warshall", b_paths_ref, b_paths);
+
+    if (PRINT_OUTPUT)
+    {
+        std::cout << "Tiramisu " << std::endl;
+        print_buffer(b_paths);
+        std::cout << "Reference " << std::endl;
+        print_buffer(b_paths_ref);
+    }
+
+    return 0;
+}
